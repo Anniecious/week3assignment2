34d8e1378532c554f8d62839e9b7d485783304ec
Anniecious <133226290+Anniecious@users.noreply.github.com>
2023-05-25 21:54:13 +0100

    Create script.php

---
+++ script.php
@@ -0,0 +1,14 @@
+<?php
+
+$name = "Ivo Hannah";
+$regnumber = "2021/HND/33173/CS";
+$soo = "Ebonyi State";
+$topic = "Design and implementation of AI metal chatbot for depressed patients A case study of  depressed patients in Enugu";
+
+
+echo "Name: $name<br>";
+echo "Regnumber: $regnumber<br>";
+echo "State of origin: $soo<br>";
+echo "Project topic: $topic<br>";
+
+?>
